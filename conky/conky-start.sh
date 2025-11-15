80c55d05fce27622691388355f88972a9038d979
ktt1973 <166073244+ktt1973@users.noreply.github.com>
2025-11-15 13:07:00 +0100

    Increase sleep duration before starting conky processes

--- conky/conky-start.sh
+++ conky/conky-start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 
 # Let the desktop load for a few seconds before running
-sleep 3
+sleep 5
 
 # Kill all current conky processes.
 killall conky
 
 conky -c /usr/lib/guideos-conky/GuideOS &
-sleep 2 && conky -c /usr/lib/guideos-conky/GuideOS-Time &
+sleep 5 && conky -c /usr/lib/guideos-conky/GuideOS-Time &
